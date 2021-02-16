0612e232be60aaedc29b76557d774df6b8888fa5
anliujun <anliujun@lvmama.com>
2021-02-16 22:20:12 +0800

    提交

--- admin/config/dev.env.js
+++ admin/config/dev.env.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
   SITE_NAME: '"papaqi电商商城"',
-  BASE_API: '"//api.papaqi.com/api/v1/admin/"',
+  BASE_API: '"http://api.papaqi.com/api/v1/admin/"',
 }
