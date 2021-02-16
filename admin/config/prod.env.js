8933cd235562e30929478150c4dddbc3fc9e6068
anliujun <anliujun@lvmama.com>
2021-02-16 22:17:54 +0800

    配置文件修改

--- admin/config/prod.env.js
+++ admin/config/prod.env.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
   SITE_NAME: '"papaqi电商商城"',
-  BASE_API: '"http://api.papaqi.com/api/v1/admin/"',
+  BASE_API: '"//www.papaqi.com/api/v1/admin/"',
 }
